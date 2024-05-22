f9f08e54054e904503b4492119754b34109e4c1b
Derrick-Asiedu-Aboagye <derrickasiedu82@gmail.com>
2024-05-22 17:35:48 +0200

    Java application Changeed to only output user-selected fields from the XML file

--- XMLReader.java
+++ XMLReader.java
@@ -1,54 +1,61 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamConstants;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
+import java.util.*;
+
+import javax.xml.stream.*;
 
 public class XMLReader {
     public static void main(String[] args) {
         try {
+            // Open the XML file
             FileInputStream fileInputStream = new FileInputStream(new File("data.xml"));
             XMLInputFactory inputFactory = XMLInputFactory.newInstance();
             XMLStreamReader xmlStreamReader = inputFactory.createXMLStreamReader(fileInputStream);
 
-            List<String> dataList = new ArrayList<>();
-            String data = "";
+            // Get user-selected fields
+            List<String> selectedFields = getSelectedFields();
+
+            // Read the XML file and extract selected fields
+            Map<String, String> fieldValues = new HashMap<>();
             while (xmlStreamReader.hasNext()) {
                 int event = xmlStreamReader.next();
                 switch (event) {
                     case XMLStreamConstants.START_ELEMENT:
-                        System.out.println("Start Element: " + xmlStreamReader.getLocalName());
-                        if (xmlStreamReader.getLocalName().equals("data")) {
-                            data = "";
-                        }
-                        break;
-                    case XMLStreamConstants.CHARACTERS:
-                        System.out.println("Characters: " + xmlStreamReader.getText());
-                        data += xmlStreamReader.getText().trim();
-                        break;
-                    case XMLStreamConstants.END_ELEMENT:
-                        System.out.println("End Element: " + xmlStreamReader.getLocalName());
-                        if (xmlStreamReader.getLocalName().equals("data")) {
-                            dataList.add(data);
+                        String elementName = xmlStreamReader.getLocalName();
+                        if (selectedFields.contains(elementName)) {
+                            xmlStreamReader.next(); // Move to character event
+                            String value = xmlStreamReader.getText().trim();
+                            fieldValues.put(elementName, value);
                         }
                         break;
                 }
             }
 
-            // Print the data read from XML
-            for (String item : dataList) {
-                System.out.println(item);
+            // Print the selected field values
+            System.out.println("Selected Field Values:");
+            for (String field : selectedFields) {
+                String value = fieldValues.get(field);
+                System.out.println(field + ": " + value);
             }
 
             // Close the streams
             xmlStreamReader.close();
-            // fileInputStream.close();
         } catch (FileNotFoundException | XMLStreamException e) {
             e.printStackTrace();
         }
+    }
+
+    private static List<String> getSelectedFields() {
+        Scanner scanner = new Scanner(System.in);
+        List<String> selectedFields = new ArrayList<>();
+        System.out.println("Enter the fields you want to extract (comma-separated):");
+        String input = scanner.nextLine();
+        String[] fields = input.split(",");
+        for (String field : fields) {
+            selectedFields.add(field.trim());
+        }
+        scanner.close();
+        return selectedFields;
     }
 }
